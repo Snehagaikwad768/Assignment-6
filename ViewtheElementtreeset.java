b80873042098f6ed99d497703954fbb5beefdc0a
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-08-04 16:45:13 +0530

    Add files via upload

---
+++ ViewtheElementtreeset.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Appendhashset;
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.TreeSet;
+import java.util.Iterator;
+public class ViewtheElementtreeset {
+  
+  public static void main(String[] args) {
+    // create an empty tree set
+     TreeSet<String> t_set = new TreeSet<String>();
+   // use add() method to add values in the tree set
+          t_set.add("Red");
+          t_set.add("Green");
+          t_set.add("Black");
+          t_set.add("Pink");
+          t_set.add("orange");
+     // print original list
+   System.out.println("Original tree set:" + t_set);  
+     Iterator it = t_set.descendingIterator();
+     // Print list elements in reverse order
+     System.out.println("Elements in Reverse Order:");
+     while (it.hasNext()) {
+        System.out.println(it.next());
+     }
+  }
+ }
+
