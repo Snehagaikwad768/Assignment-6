d90651d9074ff36827d98c449795346c9e8aee9d
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-08-04 16:22:14 +0530

    Add files via upload

---
+++ Appendhashset.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Appendhashset;
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.HashSet;
+public class Appendhashset {
+    public static void main(String[] args) {
+     // Create a empty hash set
+     HashSet<String> h_set = new HashSet<String>();
+   // use add() method to add values in the hash set
+          h_set.add("Red");
+          h_set.add("Green");
+          h_set.add("Black");
+          h_set.add("White");
+          h_set.add("Pink");
+          h_set.add("Yellow");
+
+   // print the hash set
+   System.out.println("The Hash Set: " + h_set);
+   }
+}
