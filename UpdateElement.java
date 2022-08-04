fcd9458e4c1bfcdec115db714a66ad5ab58a5f74
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-08-04 15:50:37 +0530

    Add files via upload

---
+++ UpdateElement.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Iterate_through_elements_a_array_list;
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.*;
+public class UpdateElement {
+    public static void main(String[] args) {
+  // Creae a list and add some colors to the list
+  List<String> list_Strings = new ArrayList<String>();
+  list_Strings.add("Red");
+  list_Strings.add("Green");
+  list_Strings.add("Orange");
+  list_Strings.add("White");
+  list_Strings.add("Black");
+  // Print the list
+  System.out.println(list_Strings);
+  // Update the third element with "Yellow"
+  list_Strings.set(2, "Yellow");
+   // Print the list again
+  System.out.println(list_Strings);
+ }
+}
+
