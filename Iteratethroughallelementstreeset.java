ae235bc4e22b0dea3de4ce98bf2705c336fed5e0
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-08-04 16:36:19 +0530

    Add files via upload

---
+++ Iteratethroughallelementstreeset.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Appendhashset;
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.TreeSet;
+public class Iteratethroughallelementstreeset {
+     public static void main(String[] args) {
+  TreeSet<String> tree_set = new TreeSet<String>();
+  tree_set.add("Red");
+  tree_set.add("Green");
+  tree_set.add("Orange");
+  tree_set.add("White");
+  tree_set.add("Black");
+  // Print the tree list
+  for (String element : tree_set) {
+    System.out.println(element);
+    }
+ }
+}
+
