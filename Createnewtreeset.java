ae60a62ee5ca1acb2d571c2bfa68cd3393f3aeaa
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-08-04 16:33:56 +0530

    Add files via upload

---
+++ Createnewtreeset.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Appendhashset;
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.TreeSet;
+public class Createnewtreeset {
+    public static void main(String[] args) {
+  TreeSet<String> tree_set = new TreeSet<String>();
+  tree_set.add("Red");
+  tree_set.add("Green");
+  tree_set.add("Orange");
+  tree_set.add("White");
+  tree_set.add("Black");
+  System.out.println("Tree set: ");
+  System.out.println(tree_set);
+ }
+}
+
