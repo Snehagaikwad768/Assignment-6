43433f7dcf380eb9e2ff89455c829ff263be5e12
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-08-04 16:29:14 +0530

    Add files via upload

---
+++ GetthenumberofelementHashset.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Appendhashset;
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.*;
+import java.util.Iterator;
+public class GetthenumberofelementHashset {
+ 
+  public static void main(String[] args) {
+         // Create a empty hash set
+     HashSet<String> h_set = new HashSet<String>();
+   // use add() method to add values in the hash set
+          h_set.add("Red");
+          h_set.add("Green");
+          h_set.add("Black");
+          h_set.add("White");
+          h_set.add("Pink");
+          h_set.add("Yellow");
+    System.out.println("Original Hash Set: " + h_set);
+    System.out.println("Size of the Hash Set: " + h_set.size());
+   }
+}
+}
