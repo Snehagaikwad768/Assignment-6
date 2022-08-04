6e8d7b985e9d2c69ffd312373f25401a6c5cec80
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-08-04 15:37:51 +0530

    Add files via upload

---
+++ Create_new_arraylist.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.*;
+public class Create_new_arraylist {
+      public static void main(String[] args) {
+  List<String> list_Strings = new ArrayList<String>();
+  list_Strings.add("Red");
+  list_Strings.add("Green");
+  list_Strings.add("Orange");
+  list_Strings.add("White");
+  list_Strings.add("Black");
+  System.out.println(list_Strings);
+ }
+}
+
+
