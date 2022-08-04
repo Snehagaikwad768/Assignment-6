ea1fc08c8ff7d76943a3e9026c030c5d565334ac
Snehagaikwad768 <109944120+Snehagaikwad768@users.noreply.github.com>
2022-08-04 15:56:08 +0530

    Add files via upload

---
+++ Appendthespecifiedelement.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Iterate_through_elements_a_array_list;
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.util.LinkedList;
+public class Appendthespecifiedelement {
+    public static void main(String[] args) {
+     // create an empty linked list
+     LinkedList<String> l_list = new LinkedList<String>();
+   // use add() method to add values in the linked list
+          l_list.add("Red");
+          l_list.add("Green");
+          l_list.add("Black");
+          l_list.add("White");
+          l_list.add("Pink");
+          l_list.add("Yellow");
+	
+   // print the list
+   System.out.println("The linked list: " + l_list);
+   }
+}
